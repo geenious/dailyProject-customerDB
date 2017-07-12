ad3e78772b0f2cd19d5fa2ffbf9a6ebee22818f6
Jeff Irwin <jeff.irwin@live.com>
2017-07-12 17:00:11 -0500

    initial commit

---
+++ starter_files/main.js
@@ -0,0 +1,31 @@
+function companyDirectory(thing) {
+  
+  let employeeProfile = "";
+  for (let i = 0; i < customers.results.length; i++) {
+    employeeProfile += `
+    <div id="employee">
+      <img id="headshot" src=${customers.results[i].picture.large}>
+      <div id="name">
+        ${customers.results[i].name.first} ${customers.results[i].name.last}
+      </div>
+      <div id="email">
+        ${customers.results[i].email}
+      </div>
+      <div id="address">
+        ${customers.results[i].location.street}<br>
+        ${customers.results[i].location.city}, ${customers.results[i].location.state} ${customers.results[i].location.postcode}
+      </div>
+      <div id="phone">
+        ${customers.results[i].phone}
+      </div>
+      <div id="ssn">
+        ${customers.results[i].id.value}
+      </div>
+    </div>
+  `;
+
+  document.body.innerHTML = employeeProfile;
+  }
+}
+
+companyDirectory(customers);
